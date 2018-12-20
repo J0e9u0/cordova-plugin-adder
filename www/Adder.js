fbdbd12d635ef951175b53eed03d51c225a3f5b2
Joe <joe.guo@oocl.com>
2018-12-20 11:48:21 +0800

    add js function add in cordova.plugin.Adder

--- www/Adder.js
+++ www/Adder.js
@@ -1,5 +1,6 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Adder', 'coolMethod', [arg0]);
+module.exports.add = function(arg0, success, error) {
+    // exec(success, error, 'Adder', 'coolMethod', [arg0]);
+    exec(success, null, "Adder", "performAdd", [10, 20]);
 };
