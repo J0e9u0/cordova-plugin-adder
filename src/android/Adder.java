cbbcc9422e4e9be6f3ae221cad800f26bb04fc3f
Joe <joe.guo@oocl.com>
2018-12-20 09:42:34 +0800

    update for test change Adder.java add to dec

--- src/android/Adder.java
+++ src/android/Adder.java
@@ -15,7 +15,7 @@ public class Adder extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("performAdd")) {
-            int result = args.getInt(0) + args.getInt(1);
+            int result = args.getInt(0) - args.getInt(1);
             callbackContext.success("result calculated in Java: " + result);
             return true;
         }
--- www/Adder.js
+++ www/Adder.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+module.exports.coolMethod = function(arg0, success, error) {
     exec(success, error, 'Adder', 'coolMethod', [arg0]);
 };
