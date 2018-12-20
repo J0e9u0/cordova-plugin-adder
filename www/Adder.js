4e0beef2c4686604a41653fce691750da75ad4a4
Joe <joe.guo@oocl.com>
2018-12-20 13:32:53 +0800

    fix add

--- www/Adder.js
+++ www/Adder.js
@@ -1,6 +1,5 @@
 var exec = require('cordova/exec');
 
 module.exports.add = function(arg0, success, error) {
-    // exec(success, error, 'Adder', 'coolMethod', [arg0]);
-    exec(success, null, "Adder", "performAdd", [10, 20]);
+    exec(success, null, "Adder", "performAdd", arg0);
 };
